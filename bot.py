aa3ca39cb2813c7d694fb471d5e93966804c6287
meherajsaimon740-cloud <meherajsaimon740@gmail.com>
2026-03-03 14:39:33 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,7 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
 
-import os
-
-TOKEN = os.getenv("TOKEN")
-
-if not TOKEN:
-    raise ValueError("❌ Bot token not found! Make sure TOKEN variable is set in Railway.")
+TOKEN = "8074691861:AAFti_NIEmQj3HRwgT8UHSBio4_9qwkDFac"
 
 # ===============================
 # 🔥 DATABASE (EDIT ONLY THIS)
@@ -147,5 +142,6 @@ app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CallbackQueryHandler(button_handler))
 app.run_polling()
+
 
 
